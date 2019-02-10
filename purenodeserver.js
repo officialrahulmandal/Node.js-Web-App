4a15210029559386735dafd55207bc34d65ae006
Rahul Kumar Mandal <officialrahulmandal@gmail.com>
2019-02-10 18:08:35 +0530

    end -> Adding routes and logic to a pure Node.js server

--- purenodeserver.js
+++ purenodeserver.js
@@ -3,6 +3,9 @@ const url = require('url');
 
 function handler(req, res) {
     const parsedUrl =url.parse(req.url, true);
+
+    res.setHeader('x-server-date', new Date());
+
     if(parsedUrl.pathname === '/'){
         res.writeHead(200, {'Content-type':'text/plain'});
         res.write('Hello, I am a webserver !');
@@ -10,6 +13,25 @@ function handler(req, res) {
     }else if (parsedUrl.pathname === '/time'){
         res.writeHead(200, {'Content-type': 'text/plain'});
         res.write(new Date().toString());
+        return res.end();
+    }else if(parsedUrl.pathname === '/hello'){
+        const name = parsedUrl.query.name;
+        if(!name) {
+            res.writeHead(400, {'Content-type': 'text/plain'});
+            return res.end();
+    }
+    res.writeHead(200, {'Content-type':'text/plain'});
+    res.write(`Hello ${name}`);
+    return res.end();
+    }else if(parsedUrl.pathname.startsWith('/user/')) {
+        const regex = new RegExp('\/user\/(.+)');
+        const matches = regex.exec(parsedUrl.pathname);
+        if(!matches || !matches[1]) {
+        res.writeHead(400, {'Content-type':'text/plain'});
+        res.end();
+    }
+        res.writeHead(200, {'Content-type': 'text/plain'});
+        res.write(`userprofile of ${matches[1]}`);
         return res.end();
     }else {
         res.writeHead(404, {'Content-type':'text/plain'});
